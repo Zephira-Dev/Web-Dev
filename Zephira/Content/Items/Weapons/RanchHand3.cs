614b37218f08bd5c785dca782f1447bc9bd9ea19
Katelyn Valentine <168905070+KittyValentine03@users.noreply.github.com>
2024-11-18 07:59:25 -0800

    Create RanchHand3.cs

---
+++ Zephira/Content/Items/Weapons/RanchHand3.cs
@@ -0,0 +1,73 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.Audio;
+using System;
+using Zephira.Content.Projectiles;
+
+
+namespace Zephira.Items.Weapons
+{
+    public class RanchHand3 : ModItem
+	{
+       
+        //same issue as RH2
+          
+		public override void SetStaticDefaults() {
+        	
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+		}
+        
+		public override void SetDefaults() {
+			Item.damage = 300;
+			Item.DamageType = DamageClass.Ranged;
+			Item.width = 315;
+			Item.height = 250;
+			Item.useTime = 40;
+			Item.useAnimation = 48;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.noMelee = true; //so the item's animation doesn't do damage
+			Item.knockBack = 140000;
+			Item.rare = -12;
+			Item.noUseGraphic = true;
+			Item.autoReuse = true;
+			Item.shoot = ProjectileID.ChlorophyteBullet;
+			Item.shootSpeed = 16f;
+			Item.value = Item.buyPrice(gold: 1);           //The value of the weapon
+		}
+        
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			const int NumProjectiles = 8; // The number of projectiles that this gun will shoot.
+
+			for (int i = 0; i < NumProjectiles; i++) {
+				// Rotate the velocity randomly by 30 degrees at max.
+				Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(15));
+
+				// Decrease velocity randomly for nicer visuals.
+				newVelocity *= 1f - Main.rand.NextFloat(0.3f);
+
+				// Create a projectile.
+				Projectile.NewProjectileDirect(source, position, newVelocity, ProjectileID.ChlorophyteBullet, damage, knockback, player.whoAmI);
+			}
+
+			return false; // Return false because we don't want tModLoader to shoot projectile
+		}
+
+                                
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(Mod, "RanchHand");
+			recipe.AddIngredient(ItemID.ChlorophyteBar, 30);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.Register();
+		}
+		
+				
+		
+	}
+}
